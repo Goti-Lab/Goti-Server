f2f1b957572265d9e4443fcb95cba7c7f9e89250
AkiStory <sonsungjeon@gmail.com>
2026-03-03 12:29:41 +0900

    feat: #40 enum description 추가 및 rebase

--- core/src/main/java/com/goti/constants/ResaleListingStatus.java
+++ core/src/main/java/com/goti/constants/ResaleListingStatus.java
@@ -1,9 +1,14 @@
 package com.goti.constants;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum ResaleListingStatus {
-	RESELL_AVAILABLE,
-	HOLD,
-	SOLD,
-	CANCELED,
-	FROZEN
+	RESELL_AVAILABLE("리셀가능"),
+	HOLD("홀딩중"),
+	SOLD("판매됨"),
+	CANCELED("취소"),
+	FROZEN("동결");
+
+	private final String description;
 }
