5723c6437ab2c7ab25b3b99b27ae5b95d1306746
AkiStory <sonsungjeon@gmail.com>
2026-03-03 12:29:41 +0900

    feat: #40 enum description 추가 및 rebase

--- core/src/main/java/com/goti/constants/ResaleTransactionStatus.java
+++ core/src/main/java/com/goti/constants/ResaleTransactionStatus.java
@@ -1,7 +1,13 @@
 package com.goti.constants;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum ResaleTransactionStatus {
-	PENDING,
-	CONFIRMED,
-	SETTLED
+	PENDING("대기중"),
+	CONFIRMED("확인됨"),
+	SETTLED("정산됨");
+
+	private final String description;
+
 }
