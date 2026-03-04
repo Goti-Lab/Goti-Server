d236ada8601ca67324055189eb4916500fc95c8b
junsang <89866144+junsang-ee@users.noreply.github.com>
2026-03-04 17:54:18 +0900

    [FEAT] 소셜 로그인 사전 검증 서비스 및 API 구현 (#72)
    * feat: #71
    SocialVerifyRequest 생성 (사전 검증 요청 Dto)
    
    * feat: #71
    SocialVerifyResponse 생성 (Social Verify response)
    
    * feat: #71
    createSocialVerifyToken() 생성 -> socialVerifyToken 로직 구현 (기존 registrationToken 생성 로직 및 메소드 추후 제거 예정),
    
    SocialAuthService 생성 (추후 login, signup 메소드 병합 예정) -> social 검증 및 providerId payload jwt 로직 구현,
    
    SocialVerify api 구현 (in AuthController)
    
    * feat: #71
    issueState service 및 api(state 발급 - Naver, Google 한정) 구현

--- user/src/main/java/com/goti/controller/AuthController.java
+++ user/src/main/java/com/goti/controller/AuthController.java
@@ -2,14 +2,20 @@ package com.goti.controller;
 
 import com.goti.constants.OAuthProvider;
 import com.goti.dto.request.LoginRequest;
+import com.goti.dto.request.SocialVerifyRequest;
 import com.goti.dto.response.LoginResponse;
+import com.goti.dto.response.SocialVerifyResponse;
 import com.goti.global.api.ApiSuccessResponse;
+import com.goti.infra.api.dto.response.common.SocialStateResponse;
 import com.goti.service.auth.application.AuthApplicationService;
+
+import com.goti.service.auth.application.SocialAuthService;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +30,26 @@ import static com.goti.global.api.ApiSuccessResponse.wrap;
 public class AuthController {
 
 	private final AuthApplicationService authApplicationService;
+	private final SocialAuthService socialAuthService;
+
+	@GetMapping("/{provider}/state")
+	public ResponseEntity<ApiSuccessResponse<SocialStateResponse>> issueState(
+		@PathVariable OAuthProvider provider
+	) {
+		return wrap(
+			socialAuthService.issueState(provider)
+		);
+	}
+
+	@PostMapping("/{provider}/social/verify")
+	public ResponseEntity<ApiSuccessResponse<SocialVerifyResponse>> verify(
+		@PathVariable OAuthProvider provider,
+		@RequestBody @Valid SocialVerifyRequest request
+	) {
+		return wrap(
+			socialAuthService.verify(provider, request.authCode(), request.state())
+		);
+	}
 
 	@PostMapping("/{provider}/login")
 	public ResponseEntity<ApiSuccessResponse<LoginResponse>> login(
