6810e4c1be8633d11aac0b82ebe9051a0faca682
junsang <89866144+junsang-ee@users.noreply.github.com>
2026-03-05 10:50:37 +0900

    [REFACTOR] 로그인 서비스 로직 및 API 수정 (#76)
    * feat: #71
    createSocialVerifyToken() 생성 -> socialVerifyToken 로직 구현 (기존 registrationToken 생성 로직 및 메소드 추후 제거 예정),
    
    SocialAuthService 생성 (추후 login, signup 메소드 병합 예정) -> social 검증 및 providerId payload jwt 로직 구현,
    
    SocialVerify api 구현 (in AuthController)
    
    * feat: #71
    issueState service 및 api(state 발급 - Naver, Google 한정) 구현
    
    * feat: #74
    기존 login 로직 service 제거 (AuthApplicationService)
    
    * feat: #74
    MEMBER_NOT_FOUND ErrorCode 추가
    registrationToken parser -> getSocialVerifyClaims 네이밍변경 및 로직 변경
    
    * refactor: #74
    Login api 응답 구조 변경 및 요청 데이터 구조 변경
     - LoginRequest: authCode, state 제거 -> socialVerifyToken 추가
     - TokenResponse dto 추가
    login service 로직 수정
     - 기존 AuthApplicationService -> SocialAuthService 로 이관
     - 추후 refreshToken cookie 태울 예정
    기존 AuthApplicationServiceTest 추후 다시 개발 예정
    
    * refactor: #74
    서비스 테스트 일괄 중지 (추후 재 테스트 예정)

--- user/src/test/java/com/goti/service/auth/application/AuthApplicationServiceTest.java
+++ user/src/test/java/com/goti/service/auth/application/AuthApplicationServiceTest.java
@@ -30,26 +30,23 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthApplicationServiceTest {
 
 	@Autowired
-	AuthApplicationService authApplicationService;
+	SocialAuthService socialAuthService;
 
 	@Autowired
 	RedisCache redisCache;
 
 	static final String KEY_SEPARATOR = ":";
 
-	// todo : socialUserInfo 에서 받은 providerId 로 socialProvider 데이터 유무체크
+	// todo : verify 테스트
+	// todo : socialVerifyToken 으로 로그인 성공 및 테스트
 	@Test
 	@Disabled("개별적으로 테스트 시에만 @Disabled 주석 해제 후 테스트")
 	@DisplayName("method : login()")
 	void 카카오_로그인_성공() {
 		// 브라우저에서 직접 호출하여 응답받은 code 직접 기입 후 테스트
 		String code = "";
-		authApplicationService.login(
-			OAuthProvider.KAKAO, code, null
-		);
 	}
 
-	// todo : socialUserInfo 에서 받은 providerId 로 socialProvider 데이터 유무체크
 	@Test
 	@DisplayName("method : login()")
 	@Disabled("개별적으로 테스트 시에만 @Disabled 주석 해제 후 테스트")
@@ -58,12 +55,8 @@ public class AuthApplicationServiceTest {
 		// springServer (issueState API) 에서 받은 state 직접 기입 후 테스트
 		String code = "";
 		String state = "";
-		authApplicationService.login(
-			OAuthProvider.NAVER, code, state
-		);
 	}
 
-	// todo : socialUserInfo 에서 받은 providerId 로 socialProvider 데이터 유무체크
 	@Test
 	@DisplayName("method : login()")
 	@Disabled("개별적으로 테스트 시에만 @Disabled 주석 해제 후 테스트")
@@ -72,16 +65,13 @@ public class AuthApplicationServiceTest {
 		// springServer (issueState API) 에서 받은 state 직접 기입 후 테스트
 		String code = "";
 		String state = "";
-		authApplicationService.login(
-			OAuthProvider.GOOGLE, code, state
-		);
 	}
 
 	@Test
 	@DisplayName("method : issueState()")
 	void state_생성_성공() {
 		OAuthProvider provider = OAuthProvider.NAVER;
-		SocialStateResponse response = authApplicationService.issueState(provider);
+		SocialStateResponse response = socialAuthService.issueState(provider);
 		assertNotNull(response);
 		String state = response.state();
 		log.info("state :: {}", response.state());
@@ -96,31 +86,8 @@ public class AuthApplicationServiceTest {
 	void state_생성_실패_kakao() {
 		OAuthProvider provider = OAuthProvider.KAKAO;
 		assertThatThrownBy(
-			() -> authApplicationService.issueState(provider)
+			() -> socialAuthService.issueState(provider)
 		).isInstanceOf(CustomException.class)
 			.hasMessageContaining(ErrorCode.BAD_REQUEST.getMessage());
-	}
-
-	@ParameterizedTest
-	@NullAndEmptySource
-	@DisplayName("method : login() (google, naver)")
-	void 엑세스토큰_생성_실패_state_필수_소셜로그_state_null_또는_공백(String code) {
-		OAuthProvider provider = OAuthProvider.NAVER; // (or GOOGLE)
-		assertThatThrownBy(
-			() -> authApplicationService.login(provider, code, null)
-		).isInstanceOf(CustomException.class)
-			.hasMessageContaining(ErrorCode.MISSING_PARAMETER.format("state"));
-	}
-
-	@Test
-	@DisplayName("method : login() (google, naver)")
-	void 엑세스토큰_생성_실패_state_필수_소셜로그_redis_state_미존재() {
-		OAuthProvider provider = OAuthProvider.NAVER; // (or GOOGLE)
-		String code = "testCode";
-		String state = "NOT_EXISTS_STATE";
-		assertThatThrownBy(
-			() -> authApplicationService.login(provider, code, state)
-		).isInstanceOf(CustomException.class)
-			.hasMessageContaining(ErrorCode.INVALID_STATE.getMessage());
 	}
 }
