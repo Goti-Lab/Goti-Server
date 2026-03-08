1089eb88121cf66ceef66cf50ef21d680c4e9f34
AkiStory <sonsungjeon@gmail.com>
2026-03-09 00:59:14 +0900

    refactor: #104 거래내역레포지토리 내용 수정

--- resale/src/main/java/com/goti/repository/ResalePriceHistoryRepository.java
+++ resale/src/main/java/com/goti/repository/ResalePriceHistoryRepository.java
@@ -1,25 +1,20 @@
 package com.goti.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.goti.domain.entity.resale.ResalePriceHistoryEntity;
 
 public interface ResalePriceHistoryRepository extends JpaRepository<ResalePriceHistoryEntity, UUID> {
-	@Query("SELECT rph.transactionPrice FROM ResalePriceHistoryEntity rph " +
-		"WHERE rph.gameId = :gameId " +
-		"AND rph.seatId = :seatId " +
-		"AND rph.transactionDate >= :date " +
-		"ORDER BY rph.transactionTime DESC " +
-		"LIMIT 1")
-	Optional<Integer> findLatestTransactionPrice(
-		@Param("gameId") UUID gameId,
-		@Param("seatId") UUID seatId,
-		@Param("date") LocalDate date
+	Optional<ResalePriceHistoryEntity> findFirstBySeatIdOrderByTransactionTimeDesc(UUID seatId);
+
+	List<ResalePriceHistoryEntity> findByGameIdAndGradeIdAndTransactionDateOrderByTransactionPriceAsc(
+		UUID gameId,
+		UUID gradeId,
+		LocalDate date
 	);
 }
