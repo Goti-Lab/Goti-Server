2b39120975e3da5f864d623c381a813364b91674
Josuyeon89 <138758347+Josuyeon89@users.noreply.github.com>
2026-03-08 21:17:06 +0900

    [FEAT] 좌석 구역 생성 로직 구현 (#111)
    * feat:
    
    좌석 등급 구장별 조회 서비스 구현
    
    * feat:
    
    좌석 등급 구장별 조회 API 구현
    
    * refactor:
    
    좌석 등급 생성 및 조회 로직 도메인 서비스로 분리
    
    * feat:
    
    좌석 구역 생성 요청 및 응답 DTO 추가
    
    * feat:
    
    좌석 구역 생성 서비스 로직 구현
    
    * feat:
    
    좌석 구역 생성 API 구현
    
    * fix:
    
    좌석 구역 생성 시 등급과 구장 일치 여부 검증 추가
    
    * refactor:
    
    swagger description 및 api 수정
    
    * refactor:
    
    불필요한 변수 정리
    
    * feat:
    
    구장별 좌석 구역 조회 서비스 구현
    
    * feat:
    
    구장별 좌석 구역 조회 API 구현
    
    * refactor:
    
    도메인 서비스로 분리
    
    * refactor:
    
    import 수정
    
    * refactor:
    
    좌석 구역 조회 시 seatGrade 연관 엔티티 즉시 조회 적용
    
    * refactor:
    
    get -> list로 변경
    
    * rename:
    
    applicationService -> service로 이름 변경
    
    * [FEAT] 좌석 등급 생성 로직 구현 (#108)
    
    * feat:
    
    좌석 등급 생성 요청 및 응답 DTO 추가
    
    * feat:
    
    좌석 등급 생성 서비스 로직 구현
    
    * refactor:
    
    validate 수정
    
    * feat:
    
    좌석 등급 생성 API 구현
    
    * feat:
    
    좌석 등급 생성 API swagger 추가
    
    * refactor:
    
    SeatGradeEntity에서 stadiumId+name 유니크 제약 추가
    
    * refactor:
    
    color code Pattern 검증 추가
    
    * [FEAT] 좌석 등급 조회 로직 구현 (#110)
    
    * feat:
    
    좌석 등급 구장별 조회 서비스 구현
    
    * feat:
    
    좌석 등급 구장별 조회 API 구현
    
    * refactor:
    
    좌석 등급 생성 및 조회 로직 도메인 서비스로 분리
    
    * feat:
    
    좌석 등급 생성 API swagger 추가
    
    * feat:
    
    좌석 등급 구장별 조회 서비스 구현
    
    * feat:
    
    좌석 등급 구장별 조회 API 구현
    
    * refactor:
    
    좌석 등급 생성 및 조회 로직 도메인 서비스로 분리
    
    * refactor:
    
    seat-section-get 브랜치에서 create 관련 변경내용 리베이스
    
    * refactor:
    
    좌석 구역 컨트롤러가 도메인 서비스를 직접 호출하도록 변경

--- ticketing/src/main/java/com/goti/seat/controller/SeatGradeController.java
+++ ticketing/src/main/java/com/goti/seat/controller/SeatGradeController.java
@@ -5,6 +5,10 @@ import static com.goti.global.api.ApiSuccessResponse.*;
 import java.util.List;
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.goti.global.api.ApiSuccessResponse;
 import com.goti.seat.dto.request.CreateSeatGradeRequest;
 import com.goti.seat.dto.response.SeatGradeResponse;
-import com.goti.seat.service.application.SeatGradeApplicationService;
+import com.goti.seat.service.application.SeatGradeService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -28,7 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/seat-grades")
 public class SeatGradeController {
-	private final SeatGradeApplicationService seatGradeApplicationService;
+	private final SeatGradeService seatGradeService;
 
 	@Operation(
 		summary = "좌석 등급 생성",
@@ -38,7 +40,7 @@ public class SeatGradeController {
 	public ResponseEntity<ApiSuccessResponse<SeatGradeResponse>> create(
 		@Valid @RequestBody CreateSeatGradeRequest request
 	) {
-		SeatGradeResponse response = seatGradeApplicationService.create(
+		SeatGradeResponse response = seatGradeService.create(
 			request.stadiumId(),
 			request.name(),
 			request.displayColorHex()
@@ -54,6 +56,6 @@ public class SeatGradeController {
 	public ResponseEntity<ApiSuccessResponse<List<SeatGradeResponse>>> get( // TODO: 유저 인증 추가
 		@RequestParam UUID stadiumId
 	) {
-		return wrap(seatGradeApplicationService.get(stadiumId));
+		return wrap(seatGradeService.get(stadiumId));
 	}
 }
