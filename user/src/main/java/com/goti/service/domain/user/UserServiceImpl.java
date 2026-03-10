ad1c3b277e161fb7ed03937f10d6406423d88d5e
junsang <89866144+junsang-ee@users.noreply.github.com>
2026-03-10 15:25:59 +0900

    [BUG] SecurityContext 내 인증 객체(Principal) 누락 이슈 해결 (#165)
    bug: #164
    
    UserRepository 생성 및 추가
    User 객체 누락 버그 이슈 해결 (UserService findUserById() 구현), 추가로 findUserByMobile 동시 구현

--- user/src/main/java/com/goti/service/domain/user/UserServiceImpl.java
+++ user/src/main/java/com/goti/service/domain/user/UserServiceImpl.java
@@ -1,28 +1,29 @@
 package com.goti.service.domain.user;
 
-import java.util.Optional;
-import java.util.UUID;
+import com.goti.domain.entity.user.UserEntity;
+
+import com.goti.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
-import com.goti.domain.entity.user.UserEntity;
-import com.goti.repository.MemberRepository;
-
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-	private final MemberRepository memberRepository;
+	private final UserRepository userRepository;
 
 	@Override
 	public Optional<UserEntity> findUserById(UUID userId) {
-		return memberRepository.findById(userId).map(member -> (UserEntity)member);
+		return userRepository.findById(userId);
 	}
 
 	@Override
 	public Optional<UserEntity> findUserByMobile(String mobile) {
-		return Optional.empty();
+		return userRepository.findByMobile(mobile);
 	}
 }
