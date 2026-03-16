53b3bbcc3367ff4e099dab274f8f7bfc86cd4ebc
AkiStory <sonsungjeon@gmail.com>
2026-03-16 23:56:24 +0900

    feat : #185 비동기(Async), 동기 EventListener로 구현

---
+++ resale/src/main/java/com/goti/dto/internal/ResaleOrderCreatedEvent.java
@@ -0,0 +1,16 @@
+package com.goti.dto.internal;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.goti.dto.request.ResaleTransactionItemRequest;
+
+public record ResaleOrderCreatedEvent(
+	UUID orderId,
+	UUID buyerId,
+	int totalBuyerAmount,
+	int totalBuyerFee,
+	int totalSellerFee,
+	List<ResaleTransactionItemRequest> paymentItems
+) {
+}
