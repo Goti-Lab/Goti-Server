fd299d44dc7407d91cfc5459080bb3f4e77732b9
ressKim <ressK4149@gmail.com>
2026-03-16 16:56:39 +0900

    [BUG] 모놀리식 실행 시 MSA Application 빈 중복 스캔 수정 (#202)

--- api/src/main/java/com/goti/GotiApplication.java
+++ api/src/main/java/com/goti/GotiApplication.java
@@ -3,10 +3,31 @@ package com.goti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.goti.payment.GotiPaymentApplication;
+import com.goti.resale.GotiResaleApplication;
+import com.goti.stadium.GotiStadiumApplication;
+import com.goti.ticketing.GotiTicketingApplication;
+import com.goti.user.GotiUserApplication;
+
 @ConfigurationPropertiesScan
-@SpringBootApplication(scanBasePackages = "com.goti")
+@SpringBootApplication
+@ComponentScan(
+	basePackages = "com.goti",
+	excludeFilters = @ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = {
+			GotiUserApplication.class,
+			GotiStadiumApplication.class,
+			GotiTicketingApplication.class,
+			GotiPaymentApplication.class,
+			GotiResaleApplication.class
+		}
+	)
+)
 @EnableScheduling
 public class GotiApplication {
 
