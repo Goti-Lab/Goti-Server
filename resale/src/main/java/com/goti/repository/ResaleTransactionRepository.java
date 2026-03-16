3fbd36a0049fdf610691bb29b9f16310690285af
AkiStory <sonsungjeon@gmail.com>
2026-03-16 23:11:15 +0900

    refactor : #185 ResaleTransactionRepository 에 OrderId기준 조회, 갯수 조회 로직 추가

--- resale/src/main/java/com/goti/repository/ResaleTransactionRepository.java
+++ resale/src/main/java/com/goti/repository/ResaleTransactionRepository.java
@@ -1,11 +1,17 @@
 package com.goti.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.goti.constants.ResaleTransactionStatus;
 import com.goti.domain.entity.resale.ResaleTransactionEntity;
 
 public interface ResaleTransactionRepository extends JpaRepository<ResaleTransactionEntity, UUID> {
+
+	List<ResaleTransactionEntity> findAllByResaleOrderId(UUID resaleOrderId);
+
+	int countByBuyerIdAndListing_GameIdAndTransactionStatus(UUID buyerId, UUID gameId, ResaleTransactionStatus status);
 
 }
