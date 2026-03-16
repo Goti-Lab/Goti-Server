7dd5c66d95bda0867853821d9caccafbb0fad732
AkiStory <sonsungjeon@gmail.com>
2026-03-17 00:10:01 +0900

    feat : #185 yml 수정, EnableAsync

--- api/src/main/java/com/goti/GotiApplication.java
+++ api/src/main/java/com/goti/GotiApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableAsync
 public class GotiApplication {
 
 	public static void main(String[] args) {
