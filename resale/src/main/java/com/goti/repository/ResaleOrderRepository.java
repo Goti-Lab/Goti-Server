d5d2cc32ba51ad8b8b52f79918e852d6548c06a2
AkiStory <sonsungjeon@gmail.com>
2026-03-16 23:09:49 +0900

    feat : #185 ResaleOrder Repository

---
+++ resale/src/main/java/com/goti/repository/ResaleOrderRepository.java
@@ -0,0 +1,10 @@
+package com.goti.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.goti.domain.entity.resale.ResaleOrderEntity;
+
+public interface ResaleOrderRepository extends JpaRepository<ResaleOrderEntity, UUID> {
+}
