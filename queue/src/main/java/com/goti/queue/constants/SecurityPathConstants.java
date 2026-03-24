3b64693d836468d60b7a554335bf5a48d33c03a0
suyeon89 <whtndus0809@naver.com>
2026-03-25 00:02:43 +0900

    feat:
    QueueSecurityConfig 추가

---
+++ queue/src/main/java/com/goti/queue/constants/SecurityPathConstants.java
@@ -0,0 +1,10 @@
+package com.goti.queue.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SecurityPathConstants {
+
+	public static final String[] PUBLIC_URLS = {};
+}
