7a90c687172e4f5efa4503eca8434716f2d9d965
ressKim <ressK4149@gmail.com>
2026-03-29 16:40:50 +0900

    [FEAT] Queue 커스텀 메트릭 Prometheus export + Counter 네이밍 정규화
      - micrometer-registry-prometheus 추가, /actuator/prometheus 노출
      - /actuator/prometheus SecurityConfig permitAll 등록
      - Counter 이름에서 .total suffix 제거 (Prometheus _total 중복 방지)

--- queue/src/main/java/com/goti/queue/constants/SecurityPathConstants.java
+++ queue/src/main/java/com/goti/queue/constants/SecurityPathConstants.java
@@ -6,5 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SecurityPathConstants {
 
-	public static final String[] PUBLIC_URLS = {};
+	public static final String[] PUBLIC_URLS = {
+		"/actuator/prometheus"
+	};
 }
