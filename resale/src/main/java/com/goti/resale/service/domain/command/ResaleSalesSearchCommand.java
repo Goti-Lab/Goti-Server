b0ff4526e5eb1ae79ba8fa88399307c253976a67
AkiStory <sonsungjeon@gmail.com>
2026-03-30 18:21:16 +0900

    feat : #305 DTO 리셀 판매 상태 에 대한 값 Enum 으로 변경

--- resale/src/main/java/com/goti/resale/service/domain/command/ResaleSalesSearchCommand.java
+++ resale/src/main/java/com/goti/resale/service/domain/command/ResaleSalesSearchCommand.java
@@ -3,12 +3,14 @@ package com.goti.resale.service.domain.command;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import com.goti.resale.constants.ResaleSalesStatus;
+
 public record ResaleSalesSearchCommand(
 	UUID sellerId,
 	Integer months,
 	LocalDate startDate,
 	LocalDate endDate,
-	String status,
+	ResaleSalesStatus status,
 	Integer page,
 	Integer size
 ) {
