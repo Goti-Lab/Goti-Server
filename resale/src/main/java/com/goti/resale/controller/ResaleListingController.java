17f70de14cd25ab2472ce7d89efb7987e7dbc7bb
AkiStory <sonsungjeon@gmail.com>
2026-03-30 18:10:21 +0900

    feat : #305 리셀 판매 조회 기간 설정에 대한 컨트롤러 작성

--- resale/src/main/java/com/goti/resale/controller/ResaleListingController.java
+++ resale/src/main/java/com/goti/resale/controller/ResaleListingController.java
@@ -139,18 +139,6 @@ public class ResaleListingController {
 	}
 
 	@Operation(
-		summary = "마이페이지 판매 조회",
-		description = "마이페이지의 판매중, 판매완료 개수 조회 API"
-	)
-	@GetMapping("/listings/count/listing")
-	public ResponseEntity<ApiSuccessResponse<ResaleListingMyPageCountResponse>> getCountListings(
-		@AuthenticationPrincipal(expression = "id") UUID sellerId
-	) {
-		ResaleListingMyPageCountResponse count = listingService.getCountListings(sellerId);
-		return wrap(count);
-	}
-
-	@Operation(
 		summary = "대시보드 리셀 조회",
 		description = "마이페이지 대시보드 판매중, 판매완료 갯수 조회 API"
 	)
