d6f29db77c2253e6b5eb46dc08a28029b4468578
AkiStory <sonsungjeon@gmail.com>
2026-03-31 17:56:22 +0900

    feat : #305 티켓 모듈의 리셀 api 클라이언트 마이페이지 대시보드를 위한 로직 구현

---
+++ ticketing/src/main/java/com/goti/ticketing/infra/api/TicketResaleApiClient.java
@@ -0,0 +1,34 @@
+package com.goti.ticketing.infra.api;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import com.goti.config.properties.ApiEndpointProperties;
+import com.goti.infra.api.base.BaseRestClient;
+import com.goti.ticketing.infra.api.dto.response.ResaleListingMyPageCountResponse;
+
+@Component
+public class TicketResaleApiClient extends BaseRestClient implements TicketResaleClient {
+	private final static String RESALE_GET_API = "/api/v1/internal/resales";
+	private final static String PATH_SEPARATOR = "/";
+
+	public TicketResaleApiClient(RestClient.Builder builder, ApiEndpointProperties properties) {
+		super(builder, properties.resale());
+	}
+
+	@Override
+	public ResaleListingMyPageCountResponse getMySales(UUID userId) {
+		String uri = RESALE_GET_API + PATH_SEPARATOR + "listings" + PATH_SEPARATOR + "count" + PATH_SEPARATOR + "listing";
+		return getGotiResponse(
+			uri,
+			null,
+			Map.of("sellerId", userId),
+			new ParameterizedTypeReference<>() {
+			}
+		);
+	}
+}
