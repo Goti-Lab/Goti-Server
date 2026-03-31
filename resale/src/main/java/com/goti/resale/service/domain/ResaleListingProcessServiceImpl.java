8455e876578af28f1ac7eb262aa39955b021323f
AkiStory <sonsungjeon@gmail.com>
2026-03-31 09:12:45 +0900

    refactor : #305 코드 오류 수정

--- resale/src/main/java/com/goti/resale/service/domain/ResaleListingProcessServiceImpl.java
+++ resale/src/main/java/com/goti/resale/service/domain/ResaleListingProcessServiceImpl.java
@@ -174,7 +174,7 @@ public class ResaleListingProcessServiceImpl implements ResaleListingService {
 
 		Preconditions.validate(order.getSellerId().equals(sellerId), ErrorCode.AUTH_PERMISSION_DENIED);
 
-		List<ResaleListingEntity> listings = listingRepository.findAllBySellerId(sellerId);
+		List<ResaleListingEntity> listings = listingRepository.findAllByListingOrderId(orderId);
 		ResaleRestrictionEntity restriction = restrictionService.getOrCreateRestriction(sellerId);
 
 		for (ResaleListingEntity listing : listings) {
