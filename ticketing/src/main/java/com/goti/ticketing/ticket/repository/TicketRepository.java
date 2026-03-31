4ca64e714c90292bb60f74eeec57ba795a3c6cef
AkiStory <sonsungjeon@gmail.com>
2026-03-31 12:39:05 +0900

    feat : #305 api 호출 조회를 위한 코드

--- ticketing/src/main/java/com/goti/ticketing/ticket/repository/TicketRepository.java
+++ ticketing/src/main/java/com/goti/ticketing/ticket/repository/TicketRepository.java
@@ -14,4 +14,6 @@ import com.goti.ticketing.domain.entity.ticket.TicketEntity;
 public interface TicketRepository extends JpaRepository<TicketEntity, UUID> {
 	Optional<TicketEntity> findByIdAndUserId(UUID ticketId, UUID userId);
 	List<TicketEntity> findAllByOrderItemIdIn(Collection<UUID> orderItemIds);
+
+	int countByUserIdAndGameId(UUID userId, UUID gameId);
 }
